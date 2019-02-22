4da97d5513d4312473c1b8807c246d3b52944e6d
Kenny530 <kenneth_garcia@davincischools.org>
2019-02-22 15:41:02 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+var mid1 = document.getElementById("mid1");
+var  = document.getElementById("");
+var  = document.getElementById("");
+var  = document.getElementById("");
+var  = document.getElementById("");
+var  = document.getElementById("");
+var  = document.getElementById("");
+var timer = setInterval( clueTwo, 1000);
+
