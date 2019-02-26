ca50ea6d1b5f27902e46d524ace835b2d5d91f6a
Kenny530 <kenneth_garcia@davincischools.org>
2019-02-26 13:46:09 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // https://www.w3schools.com/jsref/met_document_getelementbyid.asp
-var mid1 = document.getElementById("mid1");
-var  = document.getElementById("");
-var  = document.getElementById("");
-var  = document.getElementById("");
-var  = document.getElementById("");
-var  = document.getElementById("");
-var  = document.getElementById("");
-var timer = setInterval( clueTwo, 1000);
+//var clues = document.getElementById("clues");
+//var  mid1 = document.getElementById("mid1");
+//var  mid2 = document.getElementById("mid2");
+//var timer = setInterval( clueTwo, 1000);
+//
+var a = document.getElementById("left");
 
+function nextImage(el){
+	if (a.src.match("images/gauge1.png")){
+		a.src = "images/gauge2.png";
+	} else if (a.src.match("images/gauge2.png")){
+		a.src = "images/gauge3.png";
+	} else if (a.src.match("images/gauge3.png")){
+		a.src = "images/gauge4.png";
+	} else if (a.src.match("images/gauge4.png")) {
+		a.src = "images/gauge1.png";
+	}
+}
+
+	
